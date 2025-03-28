cb995e7bf543b0a90aac2c35acba0d1c356ee557
himanshu-rathore-5exe <himanshu..rathore@5exceptions.com>
2025-03-28 14:13:29 +0530

    implemented ios and anndroid code

--- android/src/main/java/com/truvideo/media/TruvideoSdkMediaPlugin.java
+++ android/src/main/java/com/truvideo/media/TruvideoSdkMediaPlugin.java
@@ -1,22 +1,126 @@
 package com.truvideo.media;
 
+import static com.truvideo.sdk.media.TruvideoSdkMedia.TruvideoSdkMedia;
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.util.Log;
 import com.getcapacitor.JSObject;
 import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
 import com.getcapacitor.PluginMethod;
 import com.getcapacitor.annotation.CapacitorPlugin;
+import com.google.gson.Gson;
+import com.truvideo.sdk.media.builder.TruvideoSdkMediaFileUploadRequestBuilder;
+import com.truvideo.sdk.media.interfaces.TruvideoSdkMediaCallback;
+import com.truvideo.sdk.media.interfaces.TruvideoSdkMediaFileUploadCallback;
+import com.truvideo.sdk.media.model.TruvideoSdkMediaFileUploadRequest;
+import com.truvideo.sdk.media.model.TruvideoSdkMediaMetadata;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Iterator;
+
+import kotlin.Unit;
+import truvideo.sdk.common.exceptions.TruvideoSdkException;
 
 @CapacitorPlugin(name = "TruvideoSdkMedia")
 public class TruvideoSdkMediaPlugin extends Plugin {
-
-    private TruvideoSdkMedia implementation = new TruvideoSdkMedia();
 
     @PluginMethod
     public void echo(PluginCall call) {
         String value = call.getString("value");
 
         JSObject ret = new JSObject();
-        ret.put("value", implementation.echo(value));
+        //ret.put("value", implementation.echo(value));
         call.resolve(ret);
     }
+
+    @PluginMethod
+    public void uploadMedia(PluginCall call) {
+        uploadFile(getContext(),call);
+    }
+
+    private void uploadFile(Context context, PluginCall call) {
+        try {
+            String filePath = call.getString("filePath");
+            String tag = call.getString("tag");
+            String metaData = call.getString("metaData");
+            final TruvideoSdkMediaFileUploadRequestBuilder builder = TruvideoSdkMedia.FileUploadRequestBuilder(filePath);
+            JSONObject jsonTag = new JSONObject(tag);
+            Iterator<String> keys = jsonTag.keys();
+            while (keys.hasNext()) {
+                String key = keys.next();
+                String value = jsonTag.getString(key); // Can be any type: String, Integer, Boolean, etc.
+                builder.addTag(key,value);
+            }
+
+            JSONObject jsonMetadata = new JSONObject(metaData);
+            Iterator<String> metadataKeys = jsonMetadata.keys();
+            while (metadataKeys.hasNext()) {
+                String key = metadataKeys.next();
+                String value = jsonMetadata.getString(key); // Can be any type: String, Integer, Boolean, etc.
+                builder.addMetadata(key, value);
+            }
+
+            builder.build(new TruvideoSdkMediaCallback<TruvideoSdkMediaFileUploadRequest>() {
+                @Override
+                public void onComplete(TruvideoSdkMediaFileUploadRequest data) {
+                    // File upload request created successfully
+                    // Send to upload
+                    data.upload(new TruvideoSdkMediaCallback<Unit>() {
+                                    @Override
+                                    public void onComplete(Unit data) {
+                                        // Upload started successfully
+                                    }
+                                    @Override
+                                    public void onError(@NonNull TruvideoSdkException exception) {
+                                        // Upload started with error
+                                        call.reject("API_FAILURE", "upload", exception);
+                                    }
+                                },
+                            new TruvideoSdkMediaFileUploadCallback() {
+                                @Override
+                                public void onError(@NonNull String id, @NonNull TruvideoSdkException ex) {
+                                    JSObject ret = new JSObject();
+                                    ret.put("id",id);
+                                    ret.put("error",ex);
+                                    //promise.resolve(gson.toJson(mainResponse));
+                                    sendEvent("onError", ret);
+                                }
+
+                                @Override
+                                public void onProgressChanged(@NonNull String id, float progress) {
+                                    JSObject ret = new JSObject();
+                                    ret.put("id",id);
+                                    ret.put("progress",progress * 100);
+                                    sendEvent("onProgress", ret);
+                                }
+                                @Override
+                                public void onComplete(@NonNull String id, @NonNull TruvideoSdkMediaFileUploadRequest response) {
+                                    JSObject ret = new JSObject();
+                                    ret.put("id",id);
+                                    ret.put("response",response);
+                                    call.resolve(ret);
+                                    sendEvent("onComplete", ret);
+                                }
+                            }
+                    );
+                }
+
+                @Override
+                public void onError(@NonNull TruvideoSdkException exception) {
+                    call.reject("API_FAILURE", "TruvideoSdkMediaFileUploadRequest", exception);
+                    // Handle error creating the file upload request
+                }
+            });
+
+        } catch (JSONException e) {
+            call.reject("JSON_ERROR", "Invalid JSON format", e);
+        }
+    }
+
+    public void sendEvent(String event, JSObject object) {
+        notifyListeners(event,object);
+    }
+
 }
--- ios/Sources/TruvideoSdkMediaPlugin/TruvideoSdkMediaPlugin.swift
+++ ios/Sources/TruvideoSdkMediaPlugin/TruvideoSdkMediaPlugin.swift
@@ -1,5 +1,7 @@
 import Foundation
 import Capacitor
+import Combine
+import TruvideoSdkMedia
 
 /**
  * Please read the Capacitor iOS Plugin Development Guide
@@ -9,15 +11,192 @@ import Capacitor
 public class TruvideoSdkMediaPlugin: CAPPlugin, CAPBridgedPlugin {
     public let identifier = "TruvideoSdkMediaPlugin"
     public let jsName = "TruvideoSdkMedia"
+    private var disposeBag = Set<AnyCancellable>()
     public let pluginMethods: [CAPPluginMethod] = [
         CAPPluginMethod(name: "echo", returnType: CAPPluginReturnPromise)
     ]
-    private let implementation = TruvideoSdkMedia()
+    //private let implementation = TruvideoSdkMedia()
 
     @objc func echo(_ call: CAPPluginCall) {
         let value = call.getString("value") ?? ""
         call.resolve([
-            "value": implementation.echo(value)
+            "value": value
         ])
     }
+    
+    
+    
+    @objc public func uploadMedia(_ call: CAPPluginCall) {
+        let filepath = call.getString("filepath") ?? ""
+        let tag = call.getString("tag") ?? ""
+        let metaData = call.getString("metaData") ?? ""
+        guard let fileURL = URL(string: "file://\(#filePath)") else {
+                call.reject("INVALID_URL", "The file URL is invalid", nil)
+                return
+            }
+
+            do {
+                let builder = try createFileUploadRequestBuilder(fileURL: fileURL, tag: tag, metaData: metaData)
+                try executeUploadRequest(builder: builder,call)
+            } catch {
+                call.reject("UPLOAD_ERROR", "Upload failed", error)
+            }
+        }
+        
+        private func createFileUploadRequestBuilder(fileURL: URL, tag: String, metaData: String) throws -> TruvideoSdkMedia.FileUploadRequestBuilder {
+            let builder = TruvideoSdkMedia.FileUploadRequestBuilder(fileURL: fileURL)
+            
+            // Convert tag JSON string to dictionary
+            let tagDict = try convertToDictionary(from: tag)
+            for (key, value) in tagDict {
+                builder.addTag(key, value)
+            }
+            
+            // Convert metadata JSON string to Metadata type
+            let metadataObj = try convertToDictionary(from: metaData)
+            for (key, value) in tagDict {
+                builder.addMetadata(key, value)
+            }
+            return builder
+        }
+        
+        private func executeUploadRequest(builder: TruvideoSdkMedia.FileUploadRequestBuilder,_ call: CAPPluginCall) throws {
+            let request = try builder.build()
+            
+            // Print the file upload request for debugging
+            print("fileUploadRequest: ", request.id.uuidString)
+            
+            // Completion of request
+            let completeCancellable = request.completionHandler
+                .receive(on: DispatchQueue.main)
+                .sink(receiveCompletion: { receiveCompletion in
+                    switch receiveCompletion {
+                    case .finished:
+                        print("Upload finished")
+                    case .failure(let error):
+                        // Print any errors that occur during the upload process
+                        print("Upload failure:", error)
+                        call.reject("UPLOAD_ERROR", "Upload failed", error)
+                    }
+                }, receiveValue: { uploadedResult in
+                    // Upon successful upload, retrieve the uploaded file URL
+                    let uploadedFileURL = uploadedResult.uploadedFileURL
+                    let metadataDict = uploadedResult.metadata
+                    let tags = uploadedResult.tags
+                    let transcriptionURL = uploadedResult.transcriptionURL
+                    let transcriptionLength = uploadedResult.transcriptionLength
+                    let id = request.id.uuidString
+                  print("uploadedResult: ", uploadedResult)
+                    
+                    // Send completion event
+                    let mainResponse: [String: Any] = [
+                        "id": id, // Generate a unique ID for the event
+                        "uploadedFileURL": uploadedFileURL.absoluteString,
+                        "metaData": metadataDict,
+                        "tags": tags,
+                        "transcriptionURL": transcriptionURL,
+                        "transcriptionLength": transcriptionLength
+                    ]
+                    
+                    // resolve
+                    call.resolve(["status": mainResponse])
+                    self.sendEvent(withName: "onComplete", body: mainResponse)
+                })
+            
+            // Store the completion handler in the dispose bag to avoid premature deallocation
+            completeCancellable.store(in: &disposeBag)
+            
+            // Progress of request
+            let progress = request.progressHandler
+                .receive(on: DispatchQueue.main)
+                .sink(receiveValue: { progress in
+                    let mainResponse: [String: Any] = [
+                        "id": UUID().uuidString, // Generate a unique ID for the event
+                        "progress": String(format: " %.2f %", progress.percentage * 100)
+                    ]
+                    self.sendEvent(withName: "onProgress", body: mainResponse)
+                })
+            
+            // Store the progress handler in the dispose bag to avoid premature deallocation
+            progress.store(in: &disposeBag)
+            
+            try request.upload()
+        }
+        
+        private func convertToDictionary(from jsonString: String) throws -> [String: String] {
+            guard let jsonData = jsonString.data(using: .utf8) else {
+                throw NSError(domain: "Invalid JSON string", code: 0, userInfo: nil)
+            }
+            
+            return try JSONSerialization.jsonObject(with: jsonData, options: []) as? [String: String] ?? [:]
+        }
+        
+    //    private func convertToMetadata(from jsonString: String) throws -> Metadata {
+    //        guard let jsonData = jsonString.data(using: .utf8) else {
+    //            throw NSError(domain: "Invalid JSON string", code: 0, userInfo: nil)
+    //        }
+    //
+    //        guard let metadataDict = try JSONSerialization.jsonObject(with: jsonData, options: []) as? [String: Any] else {
+    //            throw NSError(domain: "Invalid JSON format", code: 0, userInfo: nil)
+    //        }
+    //
+    //        return convertToMetadata(metadataDict)
+    //    }
+    //
+    //    private func convertToMetadata(_ dict: [String: Any]) -> Metadata {
+    //        var metadata = Metadata()
+    //        for (key, value) in dict {
+    //            if let metadataValue = convertToMetadataValue(value) {
+    //                metadata[key] = metadataValue
+    //            }
+    //        }
+    //        return metadata
+    //    }
+    //
+    //    private func convertToMetadataValue(_ value: Any) -> MetadataValue? {
+    //        if value is NSNull {
+    //            return nil
+    //        } else if let value = value as? String {
+    //            return .string(value)
+    //        } else if let value = value as? Int {
+    //            return .int(value)
+    //        } else if let value = value as? Float {
+    //            return .float(value)
+    //        } else if let value = value as? [Any] {
+    //            return .array(value.compactMap { convertToMetadataValue($0) })
+    //        } else if let value = value as? [String: Any] {
+    //            return .dictionary(convertToMetadata(value))
+    //        }
+    //        return nil
+    //    }
+    //
+    //    private func convertMetadataToDictionary(_ metadata: Metadata) -> [String: Any] {
+    //        var dict = [String: Any]()
+    //        for (key, value) in metadata {
+    //            dict[key] = convertMetadataValueToAny(value)
+    //        }
+    //        return dict
+    //    }
+    //
+    //    private func convertMetadataValueToAny(_ value: MetadataValue) -> Any {
+    //        switch value {
+    //        case .string(let stringValue):
+    //            return stringValue
+    //        case .int(let intValue):
+    //            return intValue
+    //        case .float(let floatValue):
+    //            return floatValue
+    //        case .array(let arrayValue):
+    //            return arrayValue.map { convertMetadataValueToAny($0) }
+    //        case .dictionary(let dictValue):
+    //            return convertMetadataToDictionary(dictValue)
+    //        }
+      //  }
+        
+        // Function to send events to React Native
+        private func sendEvent(withName name: String, body: [String: Any]) {
+//            guard let bridge = RCTBridge.current() else { return }
+//            bridge.eventDispatcher().sendAppEvent(withName: name, body: body)
+            self.notifyListeners(name, data: body)
+        }
 }
