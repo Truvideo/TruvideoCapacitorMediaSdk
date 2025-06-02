b954dca3cbfc736dc9d818e024488239786f222c
himanshu-rathore-5exe <himanshu.rathore@5exceptions.com>
2025-06-02 18:51:41 +0530

    fix

--- src/definitions.ts
+++ src/definitions.ts
@@ -1,4 +1,17 @@
+import { PluginListenerHandle } from "@capacitor/core";
+
+export type MediaEventMap = {
+    onProgress: { id: string; progress: number };
+    onComplete: { id: string; response: any };
+    onError: { id: string; error: any };
+};
 export interface TruvideoSdkMediaPlugin {
-  echo(options: { value: string }): Promise<{ value: string }>;
-  uploadMedia(options: { value: string }): Promise<{ value: string }>;
+    echo(options: { value: string }): Promise<{ value: string }>;
+
+    uploadMedia(options: object): Promise<{}>;
+
+    addListener<K extends keyof MediaEventMap>(
+        eventName: K,
+        listenerFunc: (event: MediaEventMap[K]) => void
+    ): Promise<PluginListenerHandle>;
 }
